9a59ead2a49c8bccd27bf125308f3da26485a791
blobaugh <ben@lobaugh.net>
2011-08-15 15:49:49 -0500

    Fixed an issue causing the WordPress scaffold not to run in local development

--- WordPress/WordPress/resources/WebRole/bin/install-php.cmd
+++ WordPress/WordPress/resources/WebRole/bin/install-php.cmd
@@ -10,6 +10,3 @@ powershell.exe .\install-php.ps1
 ECHO Installed PHP runtime. 
 
 
-
-icacls %RoleRoot%\approot /grant "Everyone":F /T
-%WINDIR%\system32\inetsrv\appcmd.exe set config -section:system.webServer/fastCgi /+"[fullPath='%ProgramFiles(x86)%\PHP\v5.3\php-cgi.exe'].environmentVariables.[name='PATH',value='%PATH%;%RoleRoot%\base\x86']" /commit:apphost 
